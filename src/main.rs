9a52d31a4404a4eba6bfbd66bff10a3899987045
Matt Freeman <m6freeman@tuta.io>
2024-12-21 21:48:43 -0500

    fix: formatting

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,19 @@ pub use constants::CITY;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let city: &str = constants::CITY;
-    let url: String = format!("https://wttr.in/{city}?format=\"%C+%t+%w+%p\"");
+    let url: String = format!("https://wttr.in/{city}?format=%C+%t+%w+%p");
     let response = minreq::get(url).send()?;
     let body: &str = response.as_str()?;
     if response.status_code == 200 {
-        println!("{}", body.replace("\"", ""));
+        if body.contains("Unknown location; please try") {
+            service_down();
+            return Ok(());
+        }
+        println!("{}", body);
     };
     Ok(())
+}
+
+fn service_down() -> () {
+    println!("wttr.in: service overburdoned")
 }
